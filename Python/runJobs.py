f08bfe589f4c0df664c0c6e3ec285df1539ff00b
josscuervo <76173386+josscuervo@users.noreply.github.com>
2020-12-23 10:57:48 +0100

    mofif test jose

--- Python/runJobs.py
+++ Python/runJobs.py
@@ -2,7 +2,7 @@ from run_AFF_FULL_FIN_TRV import run_aff
 from run_SGE_FULL_FIN_TRV import run_sge
 
 test = False
-test = True
+#test = True
 if __name__ == '__main__':
     run_aff(test)
     run_sge(test)
