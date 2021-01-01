101c0a3b778010b442fad38a826f9abee0849c96
paularnaud2 <70315271+paularnaud2@users.noreply.github.com>
2021-01-01 09:01:18 +0100

    delete tmp.sql

--- Python/runJobs.py
+++ Python/runJobs.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     from run_MAJ_PERIMETRE_FIN_TRV import run_maj_perimetre
 
     test = False
-    test = True
+    # test = True
 
     # run_maj_perimetre()
     run_aff(test)
-    # run_sge(test)
+    run_sge(test)
