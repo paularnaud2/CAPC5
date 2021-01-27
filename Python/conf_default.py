8f7ba3693f9f16b9ff5f1bd74cd5889099ab1ab7
paularnaud2 <70315271+paularnaud2@users.noreply.github.com>
2021-01-27 09:22:28 +0100

    conf_default

--- Python/conf_default.py
+++ Python/conf_default.py
@@ -9,3 +9,7 @@ ROOT_PATH = 'C:/Py/'
 # the nb of bit has to match your python version)
 ORACLE_CLIENT = 'C:/instantclient_19_6/'
 CONF_FILE = ORACLE_CLIENT + 'conf_perso.txt'
+
+# test----------------------------------------------------------------------
+TEST_ENV = 'DIRECT'
+TEST_BDD = 'CAPC5'
